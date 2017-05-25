38106076af58d851c57afa9bec0da92f92999849
Bipin <bipindr123@gmail.com>
2017-05-26 03:12:00 +0530

    Initial upload

---
+++ resultextract.py
@@ -0,0 +1,52 @@
+import urllib2
+import re
+import slate
+import pdfminer
+url="http://45.32.111.231:8080/birt/frameset?__report=mydsi/exam/Exam_Result_Sheet_dsce.rptdesign&__format=pdf&USN="
+usn = "1DS15CS00"
+i=0
+def main():
+    global i,usn
+    #for i in range(1,10):
+    #    download_file(url+usn+str(i))
+    usn = "1DS15CS0"
+    for i in range(60,99):
+        download_file(url+usn+str(i))
+    usn="1DS15CS"
+    for i in range(121,133):
+        download_file(url+usn+str(i))
+
+
+    #download_file(url+"1DS15CS015")
+def download_file(download_url):
+    response = urllib2.urlopen(download_url)
+    file = open("documentbast.pdf", 'wb')
+    file.write(response.read())
+    file.close()
+    with open('documentbast.pdf',"rb") as f:
+        doc = slate.PDF(f)
+    s1= str(doc)
+    f2 = open("mydata.txt","a")
+    res1 = re.findall(r"the Student:\\n\\n(\w+\s\w+)",s1)
+    tstr=str(res1)
+    if tstr == "[]":
+        res1 = re.findall(r"CODE\\n\\n(\w+\s\w+)",s1)
+        tstr=str(res1)
+    if tstr == "[]":
+        res1 = re.findall(r"the Student:\\n\\n(\w+)",s1)
+        tstr=str(res1)
+
+    res2 = re.findall(r'(\d\.\d*)', s1)
+    if str(res2)=="[]":
+        res2 = re.findall(r'SGPA\\n\\n(\d)', s1)
+    tstr = tstr + " "
+    tstr=tstr + str(res2)
+    tstr=re.sub("\[\'","",tstr,2)
+    tstr=re.sub("\'\]","",tstr,2)
+    print(tstr)
+    f2.write(usn+str(i)+" ")
+    f2.write(tstr)
+    f2.write("\n")
+    f2.close()
+if __name__ == "__main__":
+    main()
