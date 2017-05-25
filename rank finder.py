2a4c67b980861c4ea25ad36d6067f287ae55c550
Bipin <bipindr123@gmail.com>
2017-05-26 03:41:05 +0530

    Find rank from the file

---
+++ rank finder.py
@@ -0,0 +1,18 @@
+import re
+rt=[]
+f = open("mydata.txt","r")
+for i in f.readlines():
+    res = re.findall(r"\s(\d\.\d+)", i)
+    if str(res) == "[]":
+        res = re.findall(r"\s(\d)",i)
+    tstr=re.sub("\[\'","",str(res))
+    tstr=re.sub("\'\]","",tstr)
+    if(tstr != "[]"):
+        a = float(str(tstr))
+    rt.append(a)
+
+count=0
+for i in rt:
+    if i>8.9:
+        count+=1
+print(count)
